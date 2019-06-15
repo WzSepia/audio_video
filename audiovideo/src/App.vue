2df0fcae9bf3259fa2748063475be094b8d33770
sepia <wz45855283@163.com>
2019-06-15 21:37:29 +0800

    第一次提交

---
+++ audiovideo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<img src="./assets/logo.png" />
+		<audio controls>
+			<source src="../resource/shaolinyingxiong.mp3" />
+			您的浏览器不支持 audio 元素。
+		</audio>
+		<router-view />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'App'
+};
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
